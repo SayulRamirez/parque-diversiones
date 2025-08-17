fda0a35c4e181e4ced45b17b55599e106c7300fd
SayulRamirez <sayulramirez@gmail.com>
2025-08-17 15:40:08 -0600

    iniciando el proyecto

---
+++ src/test/java/com/saul/parque/diversiones/ParqueDiversionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saul.parque.diversiones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParqueDiversionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
